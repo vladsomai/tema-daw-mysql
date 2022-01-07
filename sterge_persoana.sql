ed52b795e86713aa937bc86fdf49a8d0bd96c628
Vlad Somai <somaivlad@gmail.com>
2022-01-07 21:58:30 +0200

    delete redundant files

--- sterge_persoana.sql
+++
@@ -1,2 +0,0 @@
-delete from persoane where NUME = "Ion Tiriac" and VARSTA = 61;
-
--- triggerPret_total.sql
+++
@@ -1,8 +0,0 @@
-drop trigger updatePretTotal;
-delimiter |
- CREATE 
-    TRIGGER  updatePretTotal
- AFTER INSERT ON produse FOR EACH ROW 
-    UPDATE produse SET PRET_TOTAL = BUCATI * PRET_BUC;
-|
-delimiter ;
